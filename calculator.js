6c6d6a53ac7b98ac69feec19e1c1999ab8e79abe
triznylaras <triznylaras@gmail.com>
2021-09-01 13:40:37 +0700

    First commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  console.log(a + b)
+}
+
+function substract(a, b) {
+  console.log(a - b)
+}
+
+function multiply(a, b) {
+  console.log(a * b)
+}
+
+function divide(a, b) {
+  console.log(a / b)
+}
+
+function operate(a, b) {
+  divide(a, b)
+}
