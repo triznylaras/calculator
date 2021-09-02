b56968a54a9184fa57063617d4ae756a641d4547
triznylaras <triznylaras@gmail.com>
2021-09-02 17:31:16 +0700

    Create buttons grid in js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
+var buttonsCont = document.getElementById("buttons-cont");
+
+
+function setupButtons(rows, cols) {
+  buttonsCont.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+  buttonsCont.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+
+  for (let i=0; i < rows*cols; i++) {
+    const button = document.createElement('div')
+    button.textContent = i + 1
+    buttonsCont.appendChild(button).className = "button-item"
+  }
+}
+
+setupButtons(5, 4)
+
 function add(a, b) {
   console.log(a + b)
 }
