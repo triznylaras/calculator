3e9b4052b8ceee5bb1145e8faab5caeddddb9422
triznylaras <triznylaras@gmail.com>
2021-09-07 14:31:57 +0700

    Add number and operator function, add display function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,35 @@
+let currentOperation = null
+let shouldResetScreen = false
+
 var display = document.getElementById("calc-display")
-var buttonsCont = document.getElementById("buttons-cont");
+var buttonsCont = document.getElementById("buttons-cont")
+var numberButtons = document.querySelectorAll('[data-number]')
+var operatorButtons = document.querySelectorAll('[data-operator]')
+
+numberButtons.forEach((button) =>
+button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+  if (display.textContent === '0' || shouldResetScreen) resetScreen
+  display.textContent += number
+}
+
+function resetScreen() {
+  display.textContent = ''
+  shouldResetScreen = false
+}
+
+function setOperation(operator) {
+  firstOperand = display.textContent
+  currentOperation = operator
+  display.textContent = `${firstOperand}${currentOperation}`
+  shouldResetScreen = true
+}
 
 function add(a, b) {
   console.log(a + b)
