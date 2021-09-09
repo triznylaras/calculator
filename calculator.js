c7c85eb9daef7932fde5b14d0d09f986aa9c07ba
triznylaras <triznylaras@gmail.com>
2021-09-09 14:44:37 +0700

    Create calculation function and add clear function

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 let currentOperation = null
 let shouldResetScreen = false
+let firstOperand = ''
+let secondOperand = ''
 
 var display = document.getElementById("calc-display")
+var firstDisplay = document.getElementById("first-display")
+var secondDisplay = document.getElementById("second-display")
 var buttonsCont = document.getElementById("buttons-cont")
 var numberButtons = document.querySelectorAll('[data-number]')
 var operatorButtons = document.querySelectorAll('[data-operator]')
+var equalsButton = document.getElementById("equals-btn")
+var pointButton = document.getElementById("point-btn")
+var clearButton = document.getElementById("clear-btn")
+var deleteButton = document.getElementById("delete-btn")
 
 numberButtons.forEach((button) =>
 button.addEventListener('click', () => appendNumber(button.textContent))
@@ -14,41 +22,77 @@ operatorButtons.forEach((button) =>
 button.addEventListener('click', () => setOperation(button.textContent))
 )
 
+equalsButton.addEventListener('click', calculate)
+clearButton.addEventListener('click', clear)
+
 function appendNumber(number) {
-  if (display.textContent === '0' || shouldResetScreen) resetScreen
-  display.textContent += number
+  if (firstDisplay.textContent === '0' || shouldResetScreen) resetScreen()
+  firstDisplay.textContent += number
 }
 
 function resetScreen() {
-  display.textContent = ''
+  firstDisplay.textContent = ''
   shouldResetScreen = false
 }
 
+function clear() {
+  firstDisplay.textContent = '0'
+  secondDisplay.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
 function setOperation(operator) {
-  firstOperand = display.textContent
+  if (currentOperation !== null) calculate()
+  firstOperand = firstDisplay.textContent
   currentOperation = operator
-  display.textContent = `${firstOperand}${currentOperation}`
+  firstDisplay.textContent = `${firstOperand}${currentOperation}`
   shouldResetScreen = true
 }
 
+function calculate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && firstDisplay.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = firstDisplay.textContent
+  secondDisplay.textContent = operate(currentOperation, firstOperand, secondOperand)
+  firstDisplay.textContent = `${firstOperand}${currentOperation}${secondOperand}`
+  currentOperation = null
+}
+
 function add(a, b) {
-  console.log(a + b)
+  return a + b
 }
 
 function substract(a, b) {
-  console.log(a - b)
+  return a - b
 }
 
 function multiply(a, b) {
-  console.log(a * b)
+  return a * b
 }
 
 function divide(a, b) {
-  console.log(a / b)
+  return a / b
 }
 
-function operate(a, b) {
+function operate(operator, a, b) {
   a = Number(a)
   b = Number(b)
   
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '-':
+      return substract(a, b)
+    case '÷':
+      return divide(a, b)
+    case 'x':
+      return multiply(a, b)
+    default:
+      return null
+  }
 }
